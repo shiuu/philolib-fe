2d8bcba644f2caaddb15bf01509b7f2105d63898
nigelzhou <nigel.zhou@sandstone.com.au>
2020-10-25 12:53:48 +1100

    Fetch data via API calls

--- src/home/Books.vue
+++ src/home/Books.vue
@@ -1,27 +1,66 @@
 <template>
   <div>
-    <h1>Catalogue</h1>
+    <h3>Catalogue</h3>
     <div class="cat">
-        
+      <div class="container">
+        <div class="row table-header">
+          <div class="col-md-5">Title</div>
+          <div class="col-md-4">Author</div>
+          <div class="col-md-3">ISBN</div>
+        </div>
+        <div class="row row-info" v-for="(book, index) in books" :key="index">
+          <div class="col-md-5">{{book.title}}</div>
+          <div class="col-md-4">{{book.author}}</div>
+          <div class="col-md-3">{{book.isbn}}</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Books',
+  data() {
+    return {
+      books: [],
+    };
+  },
+  created() {
+    this.fetchBooks();
+  },
+  methods: {
+    fetchBooks() {
+      axios.get('/api/books')
+        .then((result) => {
+          this.books = result.data;
+        })
+        .catch(console.error);
+    },
+  },
 };
 </script>
 
 <style scoped>
-  h1 {
+  h3 {
     text-align: center;
   }
   .cat {
-    margin: 0 100px;
-    font-size: 20px;
+    margin: 30px 80px;
+    font-size: 18px;
+    border-bottom: 2px solid skyblue;
   }
-  a {
-    color: palevioletred;
+  .table-header {
+    font-weight: 550;
+    padding: 12px;
+    vertical-align: top;
+    border-top: 2px solid skyblue;
+  }
+  .row-info {
+    padding: 12px;
+    vertical-align: top;
+    border-top: 1px solid #dee2e6;
   }
 </style>
--- src/home/People.vue
+++ src/home/People.vue
@@ -1,28 +1,36 @@
 <template>
   <div>
-    <h2>Users</h2>
+    <h3>Users</h3>
     <div class="users">
-      <div class="container-fluid">
+      <div class="container">
         <div class="row table-header">
-          <div class="col-md-2">Name</div>
+          <div class="col-md-4">Name</div>
           <div class="col-md-4">Email</div>
-          <div class="col-md-3">Phone</div>
+          <div class="col-md-2">Phone</div>
         </div>
         <template v-for="(person, index) in people">
           <div class="row person-info" :key="index">
-            <div class="col-md-2">{{person.name}}</div>
+            <div class="col-md-4">{{person.firstName}} {{person.lastName}}</div>
             <div class="col-md-4">{{person.email}}</div>
-            <div class="col-md-3">{{person.phone}}</div>
-            <div class="col-md-3 text-right"><button class="btn btn-outline-primary btn-sm">Items</button></div>
+            <div class="col-md-2">{{person.phone}}</div>
+            <div class="col-md-2 text-right items-btn">
+              <button class="btn btn-outline-primary btn-sm" @click="fetchItemsOnLoan(person.id)">
+                Items
+              </button>
+            </div>
           </div>
           <template v-if="person.items">
-            <div class="row">
-              <div class="col-md-4 offset-md-8">{{person.items.length || "No"}} items on loan</div>
+            <div class="row" :key="'items'+index">
+              <div class="col-md-4 offset-md-8 items text-right">
+                {{person.items.length || "No"}} items on loan
+              </div>
             </div>
-            <div class="row item-info" v-for="(item, inx) in person.items" :key="inx">
-              <div class="col-md-6 offset-md-3">{{item.title}}</div>
-              <div class="col-md-3">by {{item.author}}</div>
-            </div> 
+            <div class="row" v-for="(item, inx) in person.items" :key="'item'+inx+'for'+person.id">
+              <div class="col-md-7 offset-md-2 item-info">{{item.title}}</div>
+              <div class="col-md-3 item-info">
+                <span style="font-style:italic;">by</span> {{item.author}}
+              </div>
+            </div>
           </template>
         </template>
       </div>
@@ -31,60 +39,75 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'Books',
+  name: 'People',
   data() {
     return {
-      people: [
-        { name: 'Alice', 
-        email: 'alice@gmail.com', 
-        phone: '0412123456',
-        items: []
-        },
-        {
-          name: 'Bill', 
-          email: 'bill@hotmail.com', 
-          phone: '0412847222',
-          items: [{
-            title: "book1",
-            author: "author1",
-            isbn: "123"
-          }]
-        },
-        { name: 'Carol', email: 'carol@gmail.com', phone: '0412384229' },
-        { name: 'Nigel',
-          email: 'shiuu@gmail.com', 
-          phone: '0412788051',
-          items: [{title: "book2", author: "a2"},
-            {title: "book3", author: "a3"}
-          ]
-        }
-      ]
+      people: [],
     };
+  },
+  created() {
+    this.fetchPeople();
+  },
+  methods: {
+    fetchPeople() {
+      axios.get('/api/people')
+        .then((result) => {
+          this.people = result.data;
+        })
+        .catch(console.error);
+    },
+    fetchItemsOnLoan(personId) {
+      axios.get(`/api/books-on-loan/${personId}`)
+        .then((result) => {
+          console.log(result.data);
+          const newPeople = this.people.map((p) => {
+            if (p.id === personId) {
+              return {
+                ...p,
+                items: result.data,
+              };
+            }
+            return { ...p };
+          });
+          this.people = newPeople;
+        })
+        .catch(console.error);
+    },
   },
 };
 </script>
 
 <style scoped>
-  h2 {
+  h3 {
     text-align: center;
   }
   .users {
     margin: 30px 80px;
     font-size: 18px;
+    border-bottom: 2px solid skyblue;
   }
   .table-header {
-    font-weight: 600;
-    padding: .75rem;
+    font-weight: 550;
+    padding: 12px;
     vertical-align: top;
     border-top: 2px solid skyblue;
   }
   .person-info {
-    padding: .75rem;
+    padding: 12px;
     vertical-align: top;
     border-top: 1px solid #dee2e6;
   }
-  a {
-    color: palevioletred;
+  .items {
+    font-size: 17px;
+  }
+  .item-info {
+    font-size: 14px;
+    border-top: 1px solid #dee2e6;
+  }
+  .items-btn {
+    padding-right: 0;
   }
 </style>
