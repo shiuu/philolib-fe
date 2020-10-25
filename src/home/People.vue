a283eefa2fe75e74f5ceda6434a00b981878bbb3
nigelzhou <nigel.zhou@sandstone.com.au>
2020-10-26 00:24:24 +1100

    Make borrowed items section collapsible.

--- src/home/People.vue
+++ src/home/People.vue
@@ -8,24 +8,31 @@
           <div class="col-md-4">Email</div>
           <div class="col-md-2">Phone</div>
         </div>
-        <template v-for="(person, index) in people">
-          <div class="row person-info" :key="index">
+        <template v-for="(person, personIndex) in people">
+          <div class="row person-info" :key="personIndex">
             <div class="col-md-4">{{person.firstName}} {{person.lastName}}</div>
             <div class="col-md-4">{{person.email}}</div>
             <div class="col-md-2">{{person.phone}}</div>
             <div class="col-md-2 text-right items-btn">
-              <button class="btn btn-outline-primary btn-sm" @click="fetchItemsOnLoan(person.id)">
+              <button class="btn btn-outline-primary btn-sm" @click="fetchItemsOnLoan(personIndex)">
                 Items
               </button>
             </div>
           </div>
           <template v-if="person.items">
-            <div class="row" :key="'items'+index">
-              <div class="col-md-4 offset-md-8 items text-right">
-                {{person.items.length || "No"}} items on loan
+            <div class="row" :key="'items'+personIndex">
+              <div class="col-md-4 offset-md-8 items text-right" v-if="person.items.length"
+                @click="toggleItemsSection(personIndex)">
+                {{person.items.length}} {{person.items.length > 1 ? 'items' : 'item'}} on loan
+                <span class="collapse-arrow" v-if="itemsSectionOpen[personIndex]" >&#x25B2;</span>
+                <span class="collapse-arrow" v-else>&#x25BC;</span>
+              </div>
+              <div class="col-md-4 offset-md-8 items text-right" v-else>
+                No item on loan
               </div>
             </div>
-            <div class="row" v-for="(item, inx) in person.items" :key="'item'+inx+'for'+person.id">
+            <div class="row" v-show="itemsSectionOpen[personIndex]"
+              v-for="(item, inx) in person.items" :key="'item'+inx+'for'+person.id">
               <div class="col-md-7 offset-md-2 item-info">{{item.title}}</div>
               <div class="col-md-3 item-info">
                 <span style="font-style:italic;">by</span> {{item.author}}
@@ -46,6 +53,7 @@ export default {
   data() {
     return {
       people: [],
+      itemsSectionOpen: [],
     };
   },
   created() {
@@ -56,13 +64,14 @@ export default {
       axios.get('/api/people')
         .then((result) => {
           this.people = result.data;
+          this.itemsSectionOpen = this.people.map(_ => !!_.items);
         })
         .catch(console.error);
     },
-    fetchItemsOnLoan(personId) {
+    fetchItemsOnLoan(personIndex) {
+      const personId = this.people[personIndex].id;
       axios.get(`/api/books-on-loan/${personId}`)
         .then((result) => {
-          console.log(result.data);
           const newPeople = this.people.map((p) => {
             if (p.id === personId) {
               return {
@@ -73,8 +82,16 @@ export default {
             return { ...p };
           });
           this.people = newPeople;
+          const newItemsSectionOpen = [...this.itemsSectionOpen];
+          newItemsSectionOpen[personIndex] = !!result.data;
+          this.itemsSectionOpen = newItemsSectionOpen;
         })
         .catch(console.error);
+    },
+    toggleItemsSection(inx) {
+      const newItemsSectionOpen = [...this.itemsSectionOpen];
+      newItemsSectionOpen[inx] = !this.itemsSectionOpen[inx];
+      this.itemsSectionOpen = newItemsSectionOpen;
     },
   },
 };
@@ -102,6 +119,7 @@ export default {
   }
   .items {
     font-size: 17px;
+    color: #007bff;
   }
   .item-info {
     font-size: 14px;
@@ -111,5 +129,9 @@ export default {
   }
   .items-btn {
     padding-right: 0;
+  }
+  .collapse-arrow {
+    font-size: 12px;
+    vertical-align: text-bottom;
   }
 </style>
